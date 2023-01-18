b740e6602ca27dd2cc525fba2fb52dfc5bf3af49
mroncak <roncak@gmail.com>
2023-01-18 15:23:32 +0100

    ds

--- 120-install-core-software.sh
+++ 120-install-core-software.sh
@@ -48,6 +48,25 @@ installed_dir=$(dirname $(readlink -f $(basename `pwd`)))
 #tput sgr0
 #echo
 
+func_install() {
+    if pacman -Qi $1 &> /dev/null; then
+        tput setaf 2
+        echo "###############################################################################"
+        echo "################## The package "$1" is already installed"
+        echo "###############################################################################"
+        echo
+        tput sgr0
+    else
+        tput setaf 3
+        echo "###############################################################################"
+        echo "##################  Installing package "  $1
+        echo "###############################################################################"
+        echo
+        tput sgr0
+        sudo pacman -S --noconfirm --needed $1
+    fi
+}
+
 sudo pacman -Syy
 
 echo
@@ -58,183 +77,163 @@ echo "################################################################"
 tput sgr0
 echo
 
-######### PERSONAL INSTALL #########
-sudo pacman -S --noconfirm --needed onlyoffice-bin
-sudo pacman -S --noconfirm --needed virt-manager
-sudo pacman -S --noconfirm --needed qemu-desktop
-sudo pacman -S --noconfirm --needed qemu-emulators-full
-sudo pacman -S --noconfirm --needed iptables-nft
-sudo pacman -S --noconfirm --needed firewalld
-sudo pacman -S --noconfirm --needed ntfs-3g
-sudo pacman -S --noconfirm --needed brave-bin
-sudo pacman -S --noconfirm --needed terminator
-sudo pacman -S --noconfirm --needed freerdp
-sudo pacman -S --noconfirm --needed remmina
-sudo pacman -S --noconfirm --needed keepassxc
-sudo pacman -S --noconfirm --needed mpv
-sudo pacman -S --noconfirm --needed nextcloud-client
-sudo pacman -S --noconfirm --needed openfortivpn
-sudo pacman -S --noconfirm --needed timeshift
-sudo pacman -S --noconfirm --needed timeshift-autosnap
-sudo pacman -S --noconfirm --needed transmission-gtk
-sudo pacman -S --noconfirm --needed ranger
-sudo pacman -S --noconfirm --needed ttf-ubuntumono-nerd
-sudo pacman -S --noconfirm --needed powerline-fonts
+list=(
+onlyoffice-bin
+virt-manager
+qemu-desktop
+qemu-emulators-full
+iptables-nft
+firewalld
+ntfs-3g
+brave-bin
+terminator
+freerdp
+remmina
+keepassxc
+mpv
+nextcloud-client
+openfortivpn
+timeshift
+timeshift-autosnap
+transmission-gtk
+ranger
+ttf-ubuntumono-nerd
+powerline-fonts
+adobe-source-sans-fonts
+aic94xx-firmware
+arandr
+arc-gtk-theme
+archiso
+avahi
+awesome-terminal-fonts
+baobab
+bash-completion
+bat
+bibata-cursor-theme-bin
+catfish
+chromium
+cpuid
+curl
+dconf-editor
+dmenu
+downgrade
+duf
+evince
+expac
+feh
+file-roller
+firefox
+fish
+flameshot-git
+font-manager
+galculator
+gimp
+git
+gitahead-bin
+gitfiend
+gnome-disk-utility
+gparted
+grub-customizer
+gtop
+gvfs-smb
+hardcode-fixer-git
+hardinfo-gtk3
+hddtemp
+htop
+hw-probe
+inkscape
+insync
+kvantum
+linux-firmware-qlogic
+logrotate
+lolcat
+lshw
+man-db
+man-pages
+mlocate
+meld
+mintstick-git
+most
+neofetch
+network-manager-applet
+networkmanager-openvpn
+nitrogen
+nomacs
+noto-fonts
+ntp
+nss-mdns
+numlockx
+oh-my-zsh-git
+openresolv
+paru-bin
+pavucontrol
+playerctl
+polkit-gnome
+python-pylint
+python-pywal
+pv
+rate-mirrors-bin
+ripgrep
+rsync
+scrot
+sardi-icons
+sparklines-git
+speedtest-cli-git
+spotify
+squashfs-tools
+sublime-text-4
+surfn-icons-git
+system-config-printer
+the_platinum_searcher-bin
+the_silver_searcher
+time
+tree
+ttf-bitstream-vera
+ttf-dejavu
+ttf-droid
+ttf-hack
+ttf-inconsolata
+ttf-liberation
+ttf-roboto
+ttf-roboto-mono
+ttf-ubuntu-font-family
+upd72020x-fw
+variety
+visual-studio-code-bin
+wd719x-firmware
+wget
+wttr
+xcolor
+xdg-user-dirs
+xorg-xkill
+yay-bin
+zsh
+zsh-completions
+zsh-syntax-highlighting
+gzip
+p7zip
+unace
+unrar
+unzip
+)
 
 
-######## NEMESIS PROJECT ##########
+count=0
 
-#sudo pacman -S --noconfirm --needed ttf-wps-fonts
-#sudo pacman -S --noconfirm --needed wps-office
-#sudo pacman -S --noconfirm --needed wps-office-mime
-sudo pacman -S --noconfirm --needed adobe-source-sans-fonts
-sudo pacman -S --noconfirm --needed aic94xx-firmware
-sudo pacman -S --noconfirm --needed alacritty
-sudo pacman -S --noconfirm --needed alacritty-themes
-sudo pacman -S --noconfirm --needed arandr
-#sudo pacman -S --noconfirm --needed arc-darkest-theme-git
-sudo pacman -S --noconfirm --needed arc-gtk-theme
-sudo pacman -S --noconfirm --needed archiso
-#sudo pacman -S --noconfirm --needed asciinema
-sudo pacman -S --noconfirm --needed avahi
-sudo pacman -S --noconfirm --needed awesome-terminal-fonts
-#sudo pacman -S --noconfirm --needed ayu-theme
-sudo pacman -S --noconfirm --needed baobab
-#sudo pacman -S --noconfirm --needed base16-alacritty-git
-sudo pacman -S --noconfirm --needed bash-completion
-sudo pacman -S --noconfirm --needed bat
-sudo pacman -S --noconfirm --needed bibata-cursor-theme-bin
-sudo pacman -S --noconfirm --needed catfish
-sudo pacman -S --noconfirm --needed chromium
-sudo pacman -S --noconfirm --needed cpuid
-sudo pacman -S --noconfirm --needed curl
-sudo pacman -S --noconfirm --needed dconf-editor
-#sudo pacman -S --noconfirm --needed discord
-sudo pacman -S --noconfirm --needed dmenu
-sudo pacman -S --noconfirm --needed downgrade
-if [ ! -f /usr/bin/duf ]; then
-  sudo pacman -S --noconfirm --needed duf
-fi
-sudo pacman -S --noconfirm --needed evince
-sudo pacman -S --noconfirm --needed expac
-sudo pacman -S --noconfirm --needed feh
-sudo pacman -S --noconfirm --needed file-roller
-sudo pacman -S --noconfirm --needed firefox
-sudo pacman -S --noconfirm --needed fish
-sudo pacman -S --noconfirm --needed flameshot-git
-#sudo pacman -S --noconfirm --needed fluent-icon-theme-git
-sudo pacman -S --noconfirm --needed font-manager
-sudo pacman -S --noconfirm --needed galculator
-sudo pacman -S --noconfirm --needed gimp
-sudo pacman -S --noconfirm --needed git
-sudo pacman -S --noconfirm --needed gitahead-bin
-sudo pacman -S --noconfirm --needed gitfiend
-sudo pacman -S --noconfirm --needed gnome-disk-utility
-sudo pacman -S --noconfirm --needed gparted
-sudo pacman -S --noconfirm --needed grub-customizer
-sudo pacman -S --noconfirm --needed gtop
-sudo pacman -S --noconfirm --needed gvfs-smb
-sudo pacman -S --noconfirm --needed hardcode-fixer-git
-sudo pacman -S --noconfirm --needed hardinfo-gtk3
-sudo pacman -S --noconfirm --needed hddtemp
-sudo pacman -S --noconfirm --needed htop
-sudo pacman -S --noconfirm --needed hw-probe
-sudo pacman -S --noconfirm --needed inkscape
-sudo pacman -S --noconfirm --needed insync
-sudo pacman -S --noconfirm --needed kvantum
-sudo pacman -S --noconfirm --needed linux-firmware-qlogic
-#sudo pacman -S --noconfirm --needed lastpass
-sudo pacman -S --noconfirm --needed logrotate
-sudo pacman -S --noconfirm --needed lolcat
-sudo pacman -S --noconfirm --needed lshw
-sudo pacman -S --noconfirm --needed man-db
-sudo pacman -S --noconfirm --needed man-pages
-sudo pacman -S --noconfirm --needed mlocate
-sudo pacman -S --noconfirm --needed meld
-sudo pacman -S --noconfirm --needed mintstick-git
-sudo pacman -S --noconfirm --needed most
-sudo pacman -S --noconfirm --needed neofetch
-sudo pacman -S --noconfirm --needed network-manager-applet
-sudo pacman -S --noconfirm --needed networkmanager-openvpn
-sudo pacman -S --noconfirm --needed nitrogen
-sudo pacman -S --noconfirm --needed nomacs
-sudo pacman -S --noconfirm --needed noto-fonts
-sudo pacman -S --noconfirm --needed ntp
-sudo pacman -S --noconfirm --needed nss-mdns
-sudo pacman -S --noconfirm --needed numlockx
-sudo pacman -S --noconfirm --needed oh-my-zsh-git
-sudo pacman -S --noconfirm --needed openresolv
-sudo pacman -S --noconfirm --needed paru-bin
-sudo pacman -S --noconfirm --needed pavucontrol
-sudo pacman -S --noconfirm --needed playerctl
-sudo pacman -S --noconfirm --needed polkit-gnome
-sudo pacman -S --noconfirm --needed python-pylint
-sudo pacman -S --noconfirm --needed python-pywal
-sudo pacman -S --noconfirm --needed pv
-#sudo pacman -S --noconfirm --needed qbittorrent
-sudo pacman -S --noconfirm --needed rate-mirrors-bin
-sudo pacman -S --noconfirm --needed ripgrep
-sudo pacman -S --noconfirm --needed rsync
-sudo pacman -S --noconfirm --needed scrot
-sudo pacman -S --noconfirm --needed sardi-icons
-#sudo pacman -S --noconfirm --needed simplescreenrecorder
-sudo pacman -S --noconfirm --needed sparklines-git
-sudo pacman -S --noconfirm --needed speedtest-cli-git
-sudo pacman -S --noconfirm --needed spotify
-sudo pacman -S --noconfirm --needed squashfs-tools
-sudo pacman -S --noconfirm --needed sublime-text-4
-sudo pacman -S --noconfirm --needed surfn-icons-git
-sudo pacman -S --noconfirm --needed system-config-printer
-#sudo pacman -S --noconfirm --needed telegram-desktop
-sudo pacman -S --noconfirm --needed the_platinum_searcher-bin
-sudo pacman -S --noconfirm --needed the_silver_searcher
-sudo pacman -S --noconfirm --needed time
-sudo pacman -S --noconfirm --needed tree
-sudo pacman -S --noconfirm --needed ttf-bitstream-vera
-sudo pacman -S --noconfirm --needed ttf-dejavu
-sudo pacman -S --noconfirm --needed ttf-droid
-sudo pacman -S --noconfirm --needed ttf-hack
-sudo pacman -S --noconfirm --needed ttf-inconsolata
-sudo pacman -S --noconfirm --needed ttf-liberation
-sudo pacman -S --noconfirm --needed ttf-roboto
-sudo pacman -S --noconfirm --needed ttf-roboto-mono
-sudo pacman -S --noconfirm --needed ttf-ubuntu-font-family
-sudo pacman -S --noconfirm --needed upd72020x-fw
-sudo pacman -S --noconfirm --needed variety
-sudo pacman -S --noconfirm --needed visual-studio-code-bin
-#sudo pacman -S --noconfirm --needed vivaldi
-#sudo pacman -S --noconfirm --needed vivaldi-ffmpeg-codecs
-#sudo pacman -S --noconfirm --needed vivaldi-widevine
-#sudo pacman -S --noconfirm --needed vlc
-sudo pacman -S --noconfirm --needed wd719x-firmware
-sudo pacman -S --noconfirm --needed wget
-sudo pacman -S --noconfirm --needed wttr
-sudo pacman -S --noconfirm --needed xcolor
-sudo pacman -S --noconfirm --needed xdg-user-dirs
-sudo pacman -S --noconfirm --needed xorg-xkill
-sudo pacman -S --noconfirm --needed yay-bin
-sudo pacman -S --noconfirm --needed zsh
-sudo pacman -S --noconfirm --needed zsh-completions
-sudo pacman -S --noconfirm --needed zsh-syntax-highlighting
-sudo systemctl enable avahi-daemon.service
-sudo systemctl enable ntpd.service
-sudo systemctl enable firewalld.service
-sudo systemctl enable libvirtd.service
-
-#sudo pacman -S --noconfirm --needed rxvt-unicode
-#sudo pacman -S --noconfirm --needed urxvt-fullscreen
-#sudo pacman -S --noconfirm --needed urxvt-perls
-#sudo pacman -S --noconfirm --needed urxvt-resize-font-git
-
-sudo pacman -S --noconfirm --needed gzip
-sudo pacman -S --noconfirm --needed p7zip
-sudo pacman -S --noconfirm --needed unace
-sudo pacman -S --noconfirm --needed unrar
-sudo pacman -S --noconfirm --needed unzip
+for name in "${list[@]}" ; do
+    count=$[count+1]
+    tput setaf 3;echo "Installing package nr.  "$count " " $name;tput sgr0;
+    func_install $name
+done
 
 if [ ! -f /usr/share/xsessions/plasma.desktop ]; then
   sudo pacman -S --noconfirm --needed qt5ct
 fi
+
+sudo systemctl enable avahi-daemon.service
+sudo systemctl enable ntpd.service
+sudo systemctl enable firewalld.service
+sudo systemctl enable libvirtd.service
 
 ###############################################################################################
 
