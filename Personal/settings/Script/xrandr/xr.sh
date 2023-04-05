81f998535a024b72ea03a382529bd2d50bf95bcb
mroncak <roncak@gmail.com>
2023-04-05 14:44:58 +0200

    az

--- Personal/settings/Script/xrandr/xr.sh
+++ Personal/settings/Script/xrandr/xr.sh
@@ -9,11 +9,13 @@ fi
 place=$1
 
 if [ $place == 'home' ]; then
-	xrandr --output DP-2-2 --primary --right-of DP-2-1
+	xrandr --output eDP-1 --off --output DP-2-2 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-2-1 --mode 2560x1440 --pos 0x0 --rotate normal
 elif [ $place == 'work' ]; then	
-	xrandr --output DP-2-1 --primary --left-of DP-2-2
+	xrandr --output eDP-1 --off --output DP-2-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2-2 --mode 2560x1440 --pos 2560x0 --rotate normal
+elif [ $place == 'away' ]; then
+	xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2-1 --off --output DP-2-2 --off
 else
-	echo -e "\n$(tput setaf 1)-------------------------------------------------\nNeed argument (home or work)\n-------------------------------------------------
+	echo -e "\n$(tput setaf 1)-------------------------------------------------\nNeed argument (home or work or away)\n-------------------------------------------------
 	$(tput sgr 0)\n"
 	echo -e 
 fi
